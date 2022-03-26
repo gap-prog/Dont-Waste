ae800b65eaf8426a9a664ea72b79737bbeaa09be
wcpss-gp <82526791+wcpss-gp@users.noreply.github.com>
2022-03-26 14:04:45 -0400

    Delete mysite directory

--- mysite/base/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import Listing
-
-class ListingsForm(ModelForm):
-    class Meta:
-        model = Listing
-        fields = "__all__"
-        exclude = ["lister"]
